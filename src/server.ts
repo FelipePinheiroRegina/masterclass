792632a0bb88157c8e58a5f045aba3a90eaeeb3b
Felipe Pinheiro <flppinheiro@hotmail.com>
2023-12-31 20:28:44 -0300

    .

--- src/server.ts
+++ src/server.ts
@@ -1,2 +1,15 @@
-const nome: string = 'Felipe'
-console.log(nome)
+import fastify from 'fastify'
+
+const app = fastify()
+
+app.post('/hello', () => {
+  return 'Hellow World!'
+})
+
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log('✅ Server HTTP está pronto acesse em: http://localhost:3333')
+  })
